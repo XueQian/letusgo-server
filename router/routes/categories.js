10647ba8c9ca00a4954c6e3ee9ea23eca9fca8ed
xueqian <xueqian0321@126.com>
2014-09-18 21:23:48 +0800

    simple load

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,6 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+//var redis = require('redis');
+//var client = redis.createClient();
+//
+//var category = {id: 0, name: '服装鞋包'};
+//client.lpush("categories",category,function(err,reply){
+//  //console.log(reply);
+//});
+//
+//var category1 = {id: 1, name: '手机数码'};
+//client.lpush("categories",category1,function(err,reply){
+// //console.log(category);
+//});
+//
+//var category2 = {id: 2, name: '全球美食'};
+//client.lpush("categories",category2,function(err,reply){
+//  //console.log(reply);
+//});
+//
+//var category3 = {id: 3, name: '护肤彩妆'};
+//client.lpush("categories",category3,function(err,reply){
+// // console.log(reply);
+//});
+//
+//var category4= {id: 4, name: '母婴用品'};
+//client.lpush("categories",category4,function(err,reply){
+// // console.log(reply);
+//});
+//
+//var redisClient = redis.createClient();
+//
+//var storeCategories = function(name,data){
+//  var category = JSON.stringify({name:name,data:data});
+////console.log(category);
+//  redisClient.lpush("categories",category,function(err,response){
+//
+//  });
+//
+//};
+//
+//client.on('load',function(name) {
+//  storeCategories.forEach(function(category){
+//    category = JSON.parse(category);
+//    client.emit("categories",category.name+":"+category.data);
+//  });
+//});
+
+
 router.get('/', function(req, res) {
   //TODO: Need to implement.
   res.send('Success!');
--- router/routes/items.js
+++ router/routes/items.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var redis = require('redis');
+var client = redis.createClient();
+
 router.get('/', function(req, res) {
   //TODO: Need to implement.
-  res.send('Success!');
+  client.set('message',JSON.stringify('test1'));
+  client.get('message',function (err, reply) {
+    console.log(reply);
+    res.send(reply);
+  });
 });
+
 
 module.exports = router;
