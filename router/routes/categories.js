38dbedcdf6dfbc10d621238bd2e59f787ddc6a7f
xueqian <xueqian0321@126.com>
2014-09-21 11:21:37 +0800

    modify items structure

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -4,18 +4,39 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var _ = require('lodash');
+
+var categoryList = [
+  {id: 0, name: '服装鞋包'},
+  {id: 1, name: '手机数码'},
+  {id: 2, name: '全球美食'},
+  {id: 3, name: '护肤彩妆'},
+  {id: 4, name: '母婴用品'}
+];
+
 router.get('/', function(req, res) {
-  //TODO: Need to implement.
-  var categoryList = [
-    {id: 0, name: '服装鞋包'},
-    {id: 1, name: '手机数码'},
-    {id: 2, name: '全球美食'},
-    {id: 3, name: '护肤彩妆'},
-    {id: 4, name: '母婴用品'}
-  ];
-  client.set('data',JSON.stringify(categoryList));
-  client.get('data',function (err, reply) {
+  client.set('categoryList',JSON.stringify(categoryList));
+  client.get('categoryList',function (err, reply) {
     res.send(JSON.parse(reply));
+  });
+});
+
+router.get('/:id', function (req, res) {
+  client.get('categoryList',function (err, reply) {
+    var categoryList = JSON.parse(reply);
+    var result = _.find(categoryList,function(category){
+      return category.id == req.params.id;
+    });
+    res.send(result);
+  });
+});
+
+router.post('/', function (req, res) {
+
+  var categoryList = req.params.categoryList;
+
+  client.set('categoryList', categoryList, function (err, reply) {
+    res.send(reply);
   });
 });
 
--- router/routes/items.js
+++ router/routes/items.js
@@ -4,30 +4,29 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var itemList = [
+  {barcode: 'ITEM00000', category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
+  {barcode: 'ITEM00001', category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
+  {barcode: 'ITEM00002', category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
+  {barcode: 'ITEM00003', category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
+  {barcode: 'ITEM00004', category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
+  {barcode: 'ITEM00005', category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
+];
+
 router.get('/', function(req, res) {
-  //TODO: Need to implement.
-  var itemList = [
-    {barcode: 'ITEM00000', category: '0', name: '服装1', price: 11, unit: '件'},
-    {barcode: 'ITEM00001', category: '0', name: '服装2', price: 11, unit: '件'},
-    {barcode: 'ITEM00002', category: '1', name: '手机１', price: 1111, unit: '件'},
-    {barcode: 'ITEM00003', category: '2', name: '美食１', price: 1100, unit: '件'},
-    {barcode: 'ITEM00004', category: '3', name: '护肤１', price: 101, unit: '件'},
-    {barcode: 'ITEM00005', category: '4', name: '用品１', price: 11, unit: '件'}
-  ];
-  client.set('data',JSON.stringify(itemList));
-  client.get('data',function (err, reply) {
+  client.set('itemList',JSON.stringify(itemList));
+  client.get('itemList',function (err, reply) {
     res.send(JSON.parse(reply));
   });
 });
 
-//router.post('/',function(req,res) {
-//  var itemList = {barcode:'1',name:'测试'};
-//
-//  client.set('itemList',JSON.stringify('itemList'));
-////  client.get('itemList',function (err, reply) {
-////    console.log(reply);
-////    res.send(JSON.parse(reply));
-////  });
-//});
+router.post('/', function (req, res) {
+
+  var itemList = req.param('itemList');
+
+  client.set('itemList', itemList, function (err, reply) {
+    res.send(reply);
+  });
+});
 
 module.exports = router;
