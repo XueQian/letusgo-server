e38bea963f0db2fa414d71628b319ee417382968
Xue Qian <xueqian0321@126.com>
2014-09-22 23:48:55 +0800

    modify post and get at categoryies.js and items.js

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -6,38 +6,48 @@ var client = redis.createClient();
 
 var _ = require('lodash');
 
+var categories= [
+  {id: 0, name: '服装鞋包'},
+  {id: 1, name: '手机数码'},
+  {id: 2, name: '全球美食'},
+  {id: 3, name: '护肤彩妆'},
+  {id: 4, name: '母婴用品'}
+];
+
 
 router.get('/', function(req, res) {
-  var categoryList = req.params.categoryList;
-  client.get('categoryList',function (err, reply) {
-    res.send(JSON.parse(reply));
-  });
-});
 
-router.get('/:id', function (req, res) {
-  var categoryList = req.params.categoryList;
+
   client.get('categoryList',function (err, reply) {
-    var categoryList = JSON.parse(reply);
-    var result = _.find(categoryList,function(category){
-      return category.id == req.params.id;
-    });
-    res.send(result);
+    res.send(reply);
   });
 });
 
 router.post('/', function (req, res) {
-
-  var categoryList = [
-    {id: 0, name: '服装鞋包'},
-    {id: 1, name: '手机数码'},
-    {id: 2, name: '全球美食'},
-    {id: 3, name: '护肤彩妆'},
-    {id: 4, name: '母婴用品'}
-  ];
+  var categoryList = req.body.categoryList||categories;
 
   client.set('categoryList', JSON.stringify(categoryList), function (err, reply) {
     res.send(reply);
   });
 });
+
+//
+//router.delete('/', function (req, res) {
+//  var categoryList = req.body.categoryList;
+//  client.set('categoryList', categoryList, function (err, reply) {
+//    res.send(reply);
+//  });
+//});
+//
+//router.get('/:id', function (req, res) {
+//var categoryList = req.params.categoryList;
+//  client.get('categoryList',function (err, reply) {
+//    var categoryList = JSON.parse(reply);
+//    var result = _.find(categoryList,function(category){
+//      return category.id == req.params.id;
+//    });
+//    res.send(result);
+//  });
+//});
 
 module.exports = router;
--- router/routes/items.js
+++ router/routes/items.js
@@ -4,35 +4,65 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
-//var itemList = [
-//  {barcode: 'ITEM00000', category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
-//  {barcode: 'ITEM00001', category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
-//  {barcode: 'ITEM00002', category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
-//  {barcode: 'ITEM00003', category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
-//  {barcode: 'ITEM00004', category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
-//  {barcode: 'ITEM00005', category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
-//];
+var items = [
+  {barcode: 1, category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
+  {barcode: 2, category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
+  {barcode: 3, category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
+  {barcode: 4, category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
+  {barcode: 5, category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
+  {barcode: 6, category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
+];
+
+client.set('itemList',JSON.stringify(items));
 
 router.get('/', function(req, res) {
 
-  var itemList = req.param('itemList');
   client.get('itemList',function (err, reply) {
-    res.send(JSON.parse(reply));
+    res.send(reply);
   });
 });
 
 router.post('/', function (req, res) {
-  var itemList = [
-    {barcode: 'ITEM00000', category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
-    {barcode: 'ITEM00001', category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
-    {barcode: 'ITEM00002', category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
-    {barcode: 'ITEM00003', category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
-    {barcode: 'ITEM00004', category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
-    {barcode: 'ITEM00005', category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
-  ];
-  client.set('itemList', JSON.stringify(itemList), function (err, reply) {
+  var itemList = req.body.itemList||items;
+console.log(itemList);
+  client.set('itemList',JSON.stringify(itemList) , function (err, reply) {
     res.send(reply);
   });
 });
+
+//router.delete('/:barcode',function(req,res){
+//
+//  itemList = req.params.itemList;
+////  client.get('itemList',function (err, reply) {
+////    var itemList = JSON.parse(reply);
+////    var result = _.find(itemList,function(item){
+////      return item.barcode == req.params.barcode;
+////    });
+////    res.send(result);
+////  });
+//    if(itemList.length <= req.params.barcode) {
+//      res.send('Error 404: No itemList found');
+//    }
+//
+//  itemList.splice(req.params.barcode, 1);
+//  client.set('itemList',itemList,function(err,reply){
+//    console.log(reply);
+//
+//    res.send(reply);
+//  });
+//
+//});
+//
+//router.delete('/:id', function (req, res) {
+//  itemList = req.params.itemList;
+//  client.get('itemList',function (err, reply) {
+//    var itemList = JSON.parse(reply);
+//    var result = _.find(itemList,function(item){
+//      return itemList.id == req.params.id;
+//    });
+//
+//    res.send(result);
+//  });
+//});
 
 module.exports = router;
