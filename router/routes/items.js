17a46f3ff99bd9a88af3f532b504afd253ed897f
Xue Qian <xueqian0321@126.com>
2014-09-23 18:49:43 +0800

    add delete by barcode at items.js

--- router/routes/items.js
+++ router/routes/items.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var _ = require('lodash');
+
 var items = [
   {barcode: 1, category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
   {barcode: 2, category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
@@ -30,39 +32,25 @@ console.log(itemList);
   });
 });
 
-//router.delete('/:barcode',function(req,res){
-//
-//  itemList = req.params.itemList;
-////  client.get('itemList',function (err, reply) {
-////    var itemList = JSON.parse(reply);
-////    var result = _.find(itemList,function(item){
-////      return item.barcode == req.params.barcode;
-////    });
-////    res.send(result);
-////  });
-//    if(itemList.length <= req.params.barcode) {
-//      res.send('Error 404: No itemList found');
-//    }
-//
-//  itemList.splice(req.params.barcode, 1);
-//  client.set('itemList',itemList,function(err,reply){
-//    console.log(reply);
-//
-//    res.send(reply);
-//  });
-//
-//});
-//
-//router.delete('/:id', function (req, res) {
-//  itemList = req.params.itemList;
-//  client.get('itemList',function (err, reply) {
-//    var itemList = JSON.parse(reply);
-//    var result = _.find(itemList,function(item){
-//      return itemList.id == req.params.id;
-//    });
-//
-//    res.send(result);
-//  });
-//});
+router.delete('/:barcode',function(req,res){
+
+  client.get('itemList',function (err, reply) {
+
+    var itemList = JSON.parse(reply);
+    var result = _.find(itemList,function(item){
+
+      return item.barcode === req.params.barcode;
+    });
+
+    itemList.splice(req.params.barcode, 1);
+    itemList = _.without(itemList,result);
+
+    client.set('itemList',JSON.stringify(itemList),function(err,reply){
+
+      res.send(reply);
+  });
+  });
+});
+
 
 module.exports = router;
