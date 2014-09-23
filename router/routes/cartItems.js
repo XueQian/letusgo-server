2950b23192f6baa8c67874e5bb9292c7e99c5b35
Xue Qian <xueqian0321@126.com>
2014-09-23 12:09:26 +0800

    add get and post at cartItems.js

---
+++ router/routes/cartItems.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var redis = require('redis');
+var client = redis.createClient();
+
+router.get('/', function (req, res) {
+
+  client.get('cartItems', function (err, reply) {
+    res.send(reply);
+  });
+
+});
+
+router.post('/', function (req, res) {
+
+  var cartItems = req.body.cartItems||[];
+
+  client.set('cartItems',  JSON.stringify(cartItems), function (err, reply) {
+    res.send(reply);
+  });
+
+});
+
+module.exports = router;
