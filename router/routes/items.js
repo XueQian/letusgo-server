2fccff3b3cc57ab0c7188f46ef0330790edec5ca
Xue Qian <xueqian0321@126.com>
2014-09-24 08:16:35 +0800

    change barcode to id

--- router/routes/items.js
+++ router/routes/items.js
@@ -7,12 +7,12 @@ var client = redis.createClient();
 var _ = require('lodash');
 
 var items = [
-  {barcode: 1, category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
-  {barcode: 2, category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
-  {barcode: 3, category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
-  {barcode: 4, category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
-  {barcode: 5, category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
-  {barcode: 6, category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
+  {id: 1, category:{id: 0, name: '服装鞋包'}, name: '服装1', price: 11, unit: '件'},
+  {id: 2, category:{id: 0, name: '服装鞋包'}, name: '服装2', price: 11, unit: '件'},
+  {id: 3, category:{id: 1, name: '手机数码'}, name: '手机１', price: 1111, unit: '件'},
+  {id: 4, category:{id: 2, name: '全球美食'}, name: '美食１', price: 1100, unit: '件'},
+  {id: 5, category:{id: 3, name: '护肤彩妆'}, name: '护肤１', price: 101, unit: '件'},
+  {id: 6, category:{id: 4, name: '母婴用品'}, name: '用品１', price: 11, unit: '件'}
 ];
 
 client.set('itemList',JSON.stringify(items));
@@ -32,17 +32,17 @@ console.log(itemList);
   });
 });
 
-router.delete('/:barcode',function(req,res){
+router.delete('/:id',function(req,res){
 
   client.get('itemList',function (err, reply) {
 
     var itemList = JSON.parse(reply);
     var result = _.find(itemList,function(item){
 
-      return item.barcode === req.params.barcode;
+      return item.id === req.params.id;
     });
 
-    itemList.splice(req.params.barcode, 1);
+    itemList.splice(req.params.id, 1);
     itemList = _.without(itemList,result);
 
     client.set('itemList',JSON.stringify(itemList),function(err,reply){
