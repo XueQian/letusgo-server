eb2eaeae57a095dc6963e0f5597672e4f4cf8a41
Xue Qian <xueqian0321@126.com>
2014-09-25 13:09:31 +0800

    add put at cartItems.js

--- router/routes/cartItems.js
+++ router/routes/cartItems.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var _ = require('lodash');
+
 router.get('/', function (req, res) {
 
   client.get('cartItems', function (err, reply) {
@@ -18,6 +20,27 @@ router.post('/', function (req, res) {
 
   client.set('cartItems',  JSON.stringify(cartItems), function (err, reply) {
     res.send(reply);
+  });
+
+});
+
+router.put('/:id',function(req,res) {
+  var newCartItem = req.param('cartItem');
+  var id =parseInt(req.params.id) ;
+  client.get('cartItems',function(err,reply){
+    var cartItems = JSON.parse(reply);
+
+    _.forEach(cartItems, function (cartItem, index) {
+
+      if (cartItem.item.id === id) {
+
+        cartItems[index] = newCartItem;
+      }
+    });
+
+    client.set('cartItems',JSON.stringify(cartItems),function(err,reply){
+      res.send(cartItems);
+    });
   });
 
 });
